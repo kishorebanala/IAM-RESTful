18f530edecbdec70a5ebbd84a51e6e8b11e5d5b1
Kishore Banala <kishore.meetme@gmail.com>
2016-02-24 10:07:20 -0600

    Update appconfig.py

--- appconfig.py
+++ appconfig.py
@@ -5,6 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 # Initiate Flask App
 app = Flask(__name__)
 
+# Debug
+app.debug = True
+
 localConfig = 'mysql+mysqlconnector://root:1208@127.0.0.1:3306/iam-test'
 serverConfig = 'mysql://qpnfpkbur9apgsko:z2twcuglgsaycife@l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ofkwqg81edl8d0yd'
 
