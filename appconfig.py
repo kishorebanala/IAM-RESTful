8ed6c1547b49a1096f0970e853bc40c4a27d3e32
kishore3491 <kishore.meetme@gmail.com>
2016-02-24 10:32:55 -0600

    Updated requirements to fix mysql connector issue.

--- appconfig.py
+++ appconfig.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.debug = True
 
 localConfig = 'mysql+mysqlconnector://root:1208@127.0.0.1:3306/iam-test'
-serverConfig = 'mysql+mysqlconnector://qpnfpkbur9apgsko:z2twcuglgsaycife@l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ofkwqg81edl8d0yd'
+serverConfig = 'mysql://qpnfpkbur9apgsko:z2twcuglgsaycife@l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ofkwqg81edl8d0yd'
 
 # Set database to app
 app.config['SQLALCHEMY_DATABASE_URI'] = serverConfig
--- dbconfig.py
+++ dbconfig.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 localConfig = 'mysql+mysqlconnector://root:1208@127.0.0.1:3306/iam-test'
-serverConfig = 'mysql+mysqlconnector://qpnfpkbur9apgsko:z2twcuglgsaycife@l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ofkwqg81edl8d0yd'
+serverConfig = 'mysql://qpnfpkbur9apgsko:z2twcuglgsaycife@l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ofkwqg81edl8d0yd'
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = serverConfig
